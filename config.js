e99610a58f3a0b2ff72a33271c1878c602e773db
☠︎︎𝖘𝖍𝖆𝖑𝖑𝖎➯𝖓𝖔𝖘𝖐𝖞☠ <demoninternet1@gmail.com>
2024-08-27 19:55:46 +0100

    Update config.js

--- config.js
+++ config.js
@@ -23,9 +23,9 @@ global.location = '1'; // Replace with your location panel
 //===========================//
 
 global.prefa = ['', '!', '.', ',', '🐤', '🗿'];
-global.owner = ['2348133729715'];
-global.ownMain = '2348133729715';
-global.NamaOwner = '𝚸𝚫𝚻𝚪𝚯𝚴 𝛁1 𝚩𝐔𝐆'; // Do not change 
+global.owner = ['2347045272937'];
+global.ownMain = '2347045272037';
+global.NamaOwner = 'shallinosky 𝛁1 𝚩𝐔𝐆'; // Do not change 
 global.sessionName = 'session';
 global.connect = true; // Set to false if using QR Code
 global.namabot = '𝚸𝚫𝚻𝚪𝚯𝚴 𝛁1 𝚩𝐔𝐆'; // Do not change
